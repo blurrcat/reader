5081b7b331d2b3a1585fe3d04426bd35718eb713
blurrcat <blurrcat@gmail.com>
2019-01-09 23:43:26 -0800

    implement redirections for login/logout

--- src/Pages/Home.elm
+++ src/Pages/Home.elm
@@ -26,6 +26,7 @@ import Icons
 import Json.Decode as Decode
 import Markdown
 import RemoteData exposing (WebData)
+import Route
 import Session exposing (Session)
 import Task
 
@@ -42,25 +43,35 @@ type alias Model =
 
 init : Session -> ( Model, Cmd Msg )
 init session =
-    ( { feeds = RemoteData.Loading
-      , feedItems = RemoteData.Loading
-      , selectedFeedId = Nothing
-      , selectedFeedItemId = Nothing
-      , menuActive = False
-      , session = session
-      }
-    , Cmd.batch
-        [ Feed.list
-            (PaginatedList.params { page = 1, perPage = 20 })
-            (Session.cred session)
-            FeedsResponse
-        , FeedItem.list
-            (PaginatedList.params { page = 1, perPage = 20 })
-            Nothing
-            (Session.cred session)
-            FeedItemsResponse
-        ]
-    )
+    let
+        cmd =
+            case Session.cred session of
+                Just _ ->
+                    Cmd.batch
+                        [ Feed.list
+                            (PaginatedList.params { page = 1, perPage = 20 })
+                            (Session.cred session)
+                            FeedsResponse
+                        , FeedItem.list
+                            (PaginatedList.params { page = 1, perPage = 20 })
+                            Nothing
+                            (Session.cred session)
+                            FeedItemsResponse
+                        ]
+
+                Nothing ->
+                    -- if user is not logged in, redirect to login page
+                    Route.replaceUrl (Session.navKey session) Route.Login
+    in
+        ( { feeds = RemoteData.Loading
+          , feedItems = RemoteData.Loading
+          , selectedFeedId = Nothing
+          , selectedFeedItemId = Nothing
+          , menuActive = False
+          , session = session
+          }
+        , cmd
+        )
 
 
 type Msg
@@ -72,6 +83,7 @@ type Msg
     | SelectFeedItem FeedItem.FeedItemId
     | ToggleMenuActive
     | GotSession Session
+    | Logout
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -150,7 +162,19 @@ update msg model =
                 )
 
         GotSession session ->
-            ( { model | session = session }, Cmd.none )
+            let
+                cmd =
+                    case Session.cred session of
+                        Just _ ->
+                            Cmd.none
+
+                        Nothing ->
+                            Route.replaceUrl (Session.navKey session) Route.Login
+            in
+                ( { model | session = session }, cmd )
+
+        Logout ->
+            ( model, Api.logout )
 
 
 subscriptions : Model -> Sub Msg
@@ -166,26 +190,30 @@ jumpTo itemId =
         |> Task.attempt (\_ -> Noop)
 
 
-feedView : Maybe Feed.FeedId -> Maybe Feed.FeedId -> String -> Html Msg
-feedView selectedFeedId feedId feedTitle =
+menuLinkView msg isSelected linkText =
     li
         [ class "pure-menu-item"
         , classList
             [ ( "pure-menu-selected"
-              , selectedFeedId == feedId
+              , isSelected
               )
             ]
-        , onClick (SelectFeed feedId)
+        , onClick msg
         ]
         [ span
-            [ title feedTitle
+            [ title linkText
             , class "pure-menu-link"
             , style "cursor" "pointer"
             , style "overflow" "hidden"
             , style "text-overflow" "ellipsis"
             ]
-            [ text feedTitle ]
+            [ text linkText ]
         ]
+
+
+feedView : Maybe Feed.FeedId -> Maybe Feed.FeedId -> String -> Html Msg
+feedView selectedFeedId feedId feedTitle =
+    menuLinkView (SelectFeed feedId) (selectedFeedId == feedId) feedTitle
 
 
 feedsView : Maybe Feed.FeedId -> WebData Feed.Paginated -> Html Msg
@@ -204,6 +232,9 @@ feedsView selectedFeedId feeds =
 
                 RemoteData.Success resp ->
                     let
+                        logoutLink =
+                            menuLinkView Logout False "Logout"
+
                         latestLink =
                             feedView Nothing Nothing "Latest"
 
@@ -211,7 +242,8 @@ feedsView selectedFeedId feeds =
                             resp.results
                                 |> List.map (\f -> feedView selectedFeedId (Just f.id) f.title)
                     in
-                        ul [ class "pure-menu-list" ] (latestLink :: feedLinks)
+                        ul [ class "pure-menu-list" ]
+                            (logoutLink :: latestLink :: feedLinks)
     in
         div
             [ class "pure-menu" ]
